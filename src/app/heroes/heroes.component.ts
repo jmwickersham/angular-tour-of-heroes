8272cb1ac7767cf75a3641e1437c7d4101612106
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-04-30 21:46:33 -0700

    added routing functionality

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from '../hero';
+import { Hero }        from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -10,17 +10,12 @@ import { HeroService } from '../hero.service';
 })
 
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
